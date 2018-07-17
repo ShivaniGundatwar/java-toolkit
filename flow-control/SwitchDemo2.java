977419fc847a202f9a40d303afb1e6bc8ea19ca6
Shivani Gundatwar <sgundatwar13@gmail.com>
2018-07-17 10:44:11 +0530

    Practicing swtch case using char Data type

---
+++ flow-control/SwitchDemo2.java
@@ -0,0 +1,32 @@
+public class SwitchDemo2
+{
+	
+	enum fruits { MANGO, APPLE, ORANGE, BANANA, STROWBERRY};
+
+	public static void main(String[] args){
+
+		fruits  c = fruits.MANGO;
+		switch(c){
+			case APPLE:
+				System.out.println("student 1");
+				break;
+
+			case ORANGE:
+				System.out.println("student 2");
+				break;
+
+			case MANGO:
+				System.out.println("student 3");
+				break;
+
+			case BANANA:
+				System.out.println("student 4");
+				break;
+			
+			case STROWBERRY:
+                                System.out.println("student 4");
+                                break;
+
+		}
+	}
+}
