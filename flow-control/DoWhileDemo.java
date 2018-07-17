82b698b936ecbe7c17d7ccdc0444105a7046b849
Shivani Gundatwar <sgundatwar13@gmail.com>
2018-07-17 10:04:46 +0530

    Dowhile loop exersise

---
+++ flow-control/DoWhileDemo.java
@@ -0,0 +1,14 @@
+public class DoWhileDemo
+{
+	
+	public static void main(String[] args){
+
+		boolean flag = true;
+		do{
+			System.out.println("Shivani ");
+		} while ( flag); 
+		
+		System.out.println("Hello");//unreachable to this loop if the while(true/false) or final boolean flag(true/flag)
+
+	}
+}
