caa0d2f5413ae11f4a6ac2961ee6b8dec3cf4f24
Shivani Gundatwar <sgundatwar13@gmail.com>
2018-07-17 10:26:38 +0530

    For loop Practice

---
+++ flow-control/ForLoopDemo.java
@@ -0,0 +1,17 @@
+public class ForLoopDemo {
+
+	public static void main(String[] args){
+		
+		// initialization statement
+		// condition statement 
+		// increment or decrement statement	
+		for (int i = 0 ; i<10 ; i++ ){
+			if(i==5){
+			//	break;
+				continue;
+			}
+			System.out.println(" Hello "+i);
+		}
+
+	}
+}
