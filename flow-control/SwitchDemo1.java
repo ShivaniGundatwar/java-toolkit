bda143b7e09da0a44950d9a5c3d0a9134b580fd7
Shivani Gundatwar <sgundatwar13@gmail.com>
2018-07-17 10:36:48 +0530

    Practicing switch case using int data type

---
+++ flow-control/SwitchDemo1.java
@@ -0,0 +1,13 @@
+public class SwitchDemo1
+{
+	public static void main(String[] args){
+	byte i = 1;
+	System.out.println("Shivani thought this is one : "+i);
+	switch(i){
+		case 1:System.out.println("student 1");break;
+		case 126:System.out.println("student 2");break;
+		case 3:System.out.println("student 3");break;
+		case 4:System.out.println("student 4");break;
+		}
+	}
+}
