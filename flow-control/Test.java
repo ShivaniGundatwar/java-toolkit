94b1a7ea567922d799f523ce7d3a1578e97b99ef
Shivani Gundatwar <sgundatwar13@gmail.com>
2018-08-17 07:25:52 +0530

    Test.java File for Calculator

---
+++ flow-control/Test.java
@@ -0,0 +1,19 @@
+/**
+ * Program for checking If-else condition.
+ */
+public class IfDemo {
+
+	public static void main(String[] args){
+
+		boolean flag = false;
+		//int flag = 0;
+
+		if(flag){
+			System.out.println("Hello, Good afternoon");
+		}/* else if(flag) {
+			System.out.println("Hello, Good evening");
+		}*/ else {
+			System.out.println("Hello, Good night");
+		}
+	}
+}
