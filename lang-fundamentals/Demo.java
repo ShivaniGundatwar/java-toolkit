9062928d036a9cbd6b01722aaa7686cef32c657c
Shivani Gundatwar <sgundatwar13@gmail.com>
2018-08-17 07:33:01 +0530

    demo.java file

---
+++ lang-fundamentals/Demo.java
@@ -0,0 +1,23 @@
+public class Demo {
+
+	public static void main(String[] args){
+
+		int a = 10;
+		int b = 20;
+
+		a = b;
+		char c = 'a';
+
+		a = c;
+
+
+		int[] arr = new int[2];
+
+		int[] arr2 = arr;
+	
+		char[] c1 = new char[3];
+		arr2 = c1;
+	
+
+	}
+}
